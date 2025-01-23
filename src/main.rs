95168457871242a4dffbb6f432b70010136a618d
yu-Celik <celik.yus@outlook.com>
2025-01-23 14:44:56 +0100

    #

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,17 @@ use actix_web::middleware::Logger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    println!("Serveur démarré sur http://127.0.0.1:8080");
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let host = "0.0.0.0";
+    
+    println!("Serveur démarré sur http://{}:{}", host, port);
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .service(api::analyze_urls)
     })
-    .bind("127.0.0.1:8080")?
+    .bind((host, port.parse::<u16>().unwrap()))?
     .run()
     .await
 }
